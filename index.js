7fa4045a01a9db271d6e1d875f37df8af3097d5d
Raphaël Oester <raphaeloester@gmail.com>
2024-01-15 16:20:12 +0200

    completed flow to get access token via auth code

--- index.js
+++ index.js
@@ -5,19 +5,23 @@ const axios = require('axios');
 
 const app = express();
 
+const port = 9090;
+const keycloakPort = 8082;
+const backendPort = 8080;
+
 app.use(bodyParser.urlencoded({extended: false}));
 
 // GET / is the welcome page where the user has to login 
 app.get('/', (req, res) => {
 
-    const baseURL = 'http://localhost:55109/realms/dev/protocol/openid-connect/auth';
+    const baseURL = `http://localhost:${keycloakPort}/realms/dev/protocol/openid-connect/auth`;
     const clientID = 'puppet-master';
     const state = uuid.v4();
     const nonce = uuid.v4();
     const responseMode = 'query';
     const responseType = 'code';
     const scope = 'openid';
-    const redirectURI = `http://localhost:3000/keycloak`;
+    const redirectURI = `http://localhost:${port}/keycloak`;
 
     var url = baseURL
     + '?client_id=' + encodeURIComponent(clientID)
@@ -36,19 +40,41 @@ app.get('/', (req, res) => {
 });
 
 // GET /keycloak is the callback handler the user is sent to after a successful login on keycloak 
-app.get('/keycloak', (req, res) => {
+app.get('/keycloak', async (req, res) => {
     const error = req.query['error']
     if (error != undefined) {
         res.send(`keycloak sent back an error: ${error}`)
         return
     }
-    
+
+    req.query.redirect_uri = `http://localhost:${port}/keycloak`
     let jsonBody = JSON.stringify(req.query)
     console.log(jsonBody)
-    
-    //TODO: send that to the backend through axios and get back the access token
 
-    res.send(`login succeeded`)
+    
+    let response = null
+    try {
+        response = await axios.put(
+            `http://localhost:${backendPort}/v1/public/keycloak/get_access_token_from_auth_code`,
+            jsonBody, 
+            {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            }
+        )
+    } catch (error) {
+        // console.log(error)
+        res.send(`error while getting access token: ${error}`)
+        return
+    }
+    
+    const accessToken = response.data.access_token
+    res.send(`login succeeded, access token: ${accessToken}`)
 })
 
-app.listen(3000);
+try {
+    app.listen(port);
+} catch (error) {
+    console.log(error);
+}
